b2775b0f9206a2591fd1b041d85fb31538c4314c
Ваше Ім'я <slavikfedorenko26@gmail.com>
2026-07-21 23:51:35 +0200

    force update with new code

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "976369878" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1395623569" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
